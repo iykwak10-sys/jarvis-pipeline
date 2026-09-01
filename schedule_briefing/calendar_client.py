4685cdce5a344ab50d185996ff521f61dc0ec6d7
iykwak10-sys <iykwak10@gmail.com>
2026-09-01 11:59:34 +0900

    auto(Kwaksui-Macmini): 2026-09-01 11:59

--- schedule_briefing/calendar_client.py
+++ schedule_briefing/calendar_client.py
@@ -6,6 +6,7 @@ from __future__ import annotations
 import json
 import logging
 import os
+import subprocess
 import threading
 from datetime import datetime, time, timedelta, timezone
 from zoneinfo import ZoneInfo
